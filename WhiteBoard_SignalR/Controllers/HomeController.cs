37d3fdc52119965fc476113339dd0a29b7760476
Muhammad Aamir Khan <Muhammad.4059445@talmeez.pk>
2021-04-09 14:00:24 +0500

    Add project files.

---
+++ WhiteBoard_SignalR/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhiteBoard_SignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        string url = "";
+        public IActionResult Index(string p)
+        {
+            ViewData["IsNewGroup"] = false;
+            if (string.IsNullOrWhiteSpace(p))
+            {
+
+
+                Guid g = Guid.NewGuid();
+                p = Convert.ToBase64String(g.ToByteArray());
+                p = p.Replace("=", "");
+                p = p.Replace("+", "");
+                ViewData["IsNewGroup"] = true;
+                //  ViewData["url"] = Request.Url.AbsoluteUri.ToString() + "?p=" + p;
+                url = "https://localhost:44366/";
+                ViewData["url"] = url + "?p=" + p;
+
+            }
+            else
+            {
+                //  ViewData["url"] = Request.Url.AbsoluteUri.ToString();
+                ViewData["url"] = url + "?p=" + p;
+            }
+
+            ViewData["GroupName"] = p;
+            ViewBag.GroupName = p;
+
+            return View();
+        }
+    }
+}
---
+++ WhiteBoard_SignalR/WhiteBoardHub.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WhiteBoard_SignalR
+{
+    public class WhiteBoardHub : Hub
+    {
+        public async Task JoinGroup(string groupName)
+        {
+
+            //  Groups.Add(Context.ConnectionId, groupName);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+           
+
+        }
+        public async Task JoinChat(string name, string groupName)
+        {
+
+
+            // Clients.Group(groupName).ChatJoined(name);
+            await Clients.Group(groupName).SendAsync(name);
+
+        }
+
+        public async Task SendDraw(string drawObject, string sessionId, string groupName, string name)
+        {
+            //Clients.Group(groupName).HandleDraw(drawObject, sessionId, name);
+            await Clients.Group(groupName).SendAsync("HandleDraw", drawObject, sessionId, name);
+        }
+
+        public async Task SendChat(string message, string groupName, string name)
+        {
+            //  Clients.Group(groupName).Chat(name, message);
+            await Clients.Group(groupName).SendAsync("Chat", name, message);
+        }
+    }
+}
